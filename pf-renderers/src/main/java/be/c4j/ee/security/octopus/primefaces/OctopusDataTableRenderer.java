4c4ae6d5a5c8a53368c6be087b34c2877f25aaab
Rudy De Busscher <rudy.debusscher@c4j.be>
2014-03-22 09:15:04 +0100

    Fix issue #14. Improvements/bug for PrimeFaces Column interceptor renderer

--- pf-renderers/src/main/java/be/c4j/ee/security/octopus/primefaces/OctopusDataTableRenderer.java
+++ pf-renderers/src/main/java/be/c4j/ee/security/octopus/primefaces/OctopusDataTableRenderer.java
@@ -2,12 +2,15 @@ package be.c4j.ee.security.octopus.primefaces;
 
 import be.c4j.ee.security.view.interceptor.SecuredRuntimeManager;
 import org.apache.myfaces.extensions.cdi.core.impl.util.CodiUtils;
+import org.primefaces.component.api.DynamicColumn;
 import org.primefaces.component.api.UIColumn;
+import org.primefaces.component.column.Column;
 import org.primefaces.component.datatable.DataTable;
 import org.primefaces.component.datatable.DataTableRenderer;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
 import java.io.IOException;
 
 /**
@@ -23,9 +26,32 @@ public class OctopusDataTableRenderer extends DataTableRenderer {
 
     @Override
     protected void encodeCell(FacesContext context, DataTable table, UIColumn column, String clientId, boolean selected) throws IOException {
-        if (column.isRendered() && securedRuntimeManager.allowed((UIComponent) column)) {
+        if (column.isRendered()) {
+            if (securedRuntimeManager.allowed((UIComponent) column)) {
 
-        super.encodeCell(context, table, column, clientId, selected);
+                super.encodeCell(context, table, column, clientId, selected);
+            } else {
+                renderEmptyCell(context);
+            }
         }
+    }
+
+    private void renderEmptyCell(FacesContext context) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+
+        writer.startElement("td", null);
+        writer.writeAttribute("role", "gridcell", null);
+        writer.endElement("td");
+
+    }
+
+    @Override
+    public boolean encodeRow(FacesContext context, DataTable table, String clientId, int rowIndex) throws IOException {
+        boolean result = super.encodeRow(context, table, clientId, rowIndex);
+        for (UIColumn column : table.getColumns()) {
+            securedRuntimeManager.resetRenderedStatus((UIComponent) column);
+        }
+
+        return result;
     }
 }
