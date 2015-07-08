1ba5855c397c10aec8ed1e907c6cd154bcbfa487
Rudy De Busscher <rudy.debusscher@c4j.be>
2015-07-08 12:30:26 +0200

    Alternative version for NamedPermissionFilter and NamedRoleFilter where only 1 permission needs to be satisfied. fixes #55.

---
+++ core/src/main/java/be/c4j/ee/security/role/filter/NamedRoleOneFilter.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2014-2015 Rudy De Busscher (www.c4j.be)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package be.c4j.ee.security.role.filter;
+
+import be.c4j.ee.security.role.NamedRole;
+import be.c4j.ee.security.role.RoleLookup;
+import be.c4j.ee.security.util.CDIUtil;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.filter.authz.AuthorizationFilter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ *
+ */
+public class NamedRoleOneFilter extends AuthorizationFilter {
+
+    private RoleLookup<? extends NamedRole> roleLookup;
+
+    @Override
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws
+            Exception {
+        Subject subject = getSubject(request, response);
+        String[] roles = (String[]) mappedValue;
+        checkLookup();
+
+        boolean permitted = false;
+        for (String role : roles) {
+            if (subject.isPermitted(roleLookup.getRole(role))) {
+                permitted = true;
+            }
+        }
+        return permitted;
+    }
+
+    private void checkLookup() {
+        // We can't do this in onFilterConfigSet as it is to soon.  Not available at that time
+        if (roleLookup == null) {
+            roleLookup = CDIUtil.getBeanManually(RoleLookup.class);
+        }
+    }
+}
