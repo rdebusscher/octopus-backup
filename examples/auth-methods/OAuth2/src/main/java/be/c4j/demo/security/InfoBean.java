1ddfec86cd26464cc27b4bb2f7c294bfe6df7363
Rudy De Busscher <rudy.debusscher@c4j.be>
2016-03-05 10:44:08 +0100

    Example of using multiple OAuth2 providers.

--- examples/auth-methods/OAuth2/src/main/java/be/c4j/demo/security/InfoBean.java
+++ examples/auth-methods/OAuth2/src/main/java/be/c4j/demo/security/InfoBean.java
@@ -1,7 +1,35 @@
 package be.c4j.demo.security;
 
+import be.c4j.ee.security.model.UserPrincipal;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  */
+@Model
 public class InfoBean {
+
+    @Inject
+    private UserPrincipal userPrincipal;
+
+    private List<Serializable> keys;
+
+    @PostConstruct
+    public void init() {
+        keys = new ArrayList<Serializable>(userPrincipal.getInfo().keySet());
+    }
+
+    public String getInfoValue(String key) {
+        return userPrincipal.getUserInfo(key);
+    }
+
+    public List<Serializable> getKeys() {
+        return keys;
+    }
 }
