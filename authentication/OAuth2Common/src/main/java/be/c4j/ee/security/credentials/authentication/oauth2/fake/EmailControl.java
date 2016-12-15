961627c90751463afb3f6991051562406a2de139
Rudy De Busscher <rdebusscher@c4j.be>
2016-12-15 22:13:15 +0100

    Fixes License headers.

--- authentication/OAuth2Common/src/main/java/be/c4j/ee/security/credentials/authentication/oauth2/fake/EmailControl.java
+++ authentication/OAuth2Common/src/main/java/be/c4j/ee/security/credentials/authentication/oauth2/fake/EmailControl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014-2016 Rudy De Busscher (www.c4j.be)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package be.c4j.ee.security.credentials.authentication.oauth2.fake;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -12,7 +28,6 @@ public class EmailControl {
     private static final String EMAIL_PATTERN =
             "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
                     + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-
 
     public boolean isValidEmail(String email) {
         return email != null && Pattern.matches(EMAIL_PATTERN, email);
