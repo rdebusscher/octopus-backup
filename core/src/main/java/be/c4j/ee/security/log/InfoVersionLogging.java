a717b9d0e16282719cf6a0b9cdf5a7b66a8a192f
Rudy De Busscher <rdebusscher@gmail.com>
2017-02-22 09:13:15 +0100

    Fix Issue with version logging.

--- core/src/main/java/be/c4j/ee/security/log/InfoVersionLogging.java
+++ core/src/main/java/be/c4j/ee/security/log/InfoVersionLogging.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014-2017 Rudy De Busscher (www.c4j.be)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package be.c4j.ee.security.log;
 
 import org.slf4j.Logger;
@@ -30,13 +45,17 @@ public class InfoVersionLogging {
         try {
             URL manifestFile = findManifestFile();
 
-            InputStream resourceAsStream = manifestFile.openStream();
-            if (resourceAsStream != null) {
-                properties.load(resourceAsStream);
-            }
+            if (manifestFile != null) {
+                InputStream resourceAsStream = manifestFile.openStream();
+                if (resourceAsStream != null) {
+                    properties.load(resourceAsStream);
+                }
 
-            if (resourceAsStream != null) {
-                resourceAsStream.close();
+                if (resourceAsStream != null) {
+                    resourceAsStream.close();
+                }
+            } else {
+                logger.warn("Unable to find manifest file of Octopus Core module");
             }
 
         } catch (IOException e) {
