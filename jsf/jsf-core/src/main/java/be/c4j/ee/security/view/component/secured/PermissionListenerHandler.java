18651f45e1bf71b7c6961e0a8823b7d1405928ef
Rudy De Busscher <rdebusscher@gmail.com>
2018-02-03 16:13:21 +0100

    Improve handling of listener attribute of securedListener tag.

--- jsf/jsf-core/src/main/java/be/c4j/ee/security/view/component/secured/PermissionListenerHandler.java
+++ jsf/jsf-core/src/main/java/be/c4j/ee/security/view/component/secured/PermissionListenerHandler.java
@@ -16,10 +16,13 @@
 package be.c4j.ee.security.view.component.secured;
 
 import be.c4j.ee.security.util.JsfUtils;
+import be.c4j.ee.security.view.component.OctopusComponentUsageException;
 
+import javax.el.ELException;
 import javax.el.MethodExpression;
 import javax.faces.component.UIComponent;
 import javax.faces.view.facelets.FaceletContext;
+import javax.faces.view.facelets.TagAttribute;
 import javax.faces.view.facelets.TagConfig;
 
 /**
@@ -35,8 +38,16 @@ public class PermissionListenerHandler extends OctopusTagHandler {
     void handleSecurity(FaceletContext ctx, UIComponent parent, SecuredComponentData securedComponentData) {
         if (octopusHandlerHelper.hasAccess(securedComponentData)) {
 
-            MethodExpression listener = JsfUtils.createMethodExpression(getAttribute("listener").getValue(), Void.class, UIComponent.class);
-            listener.invoke(ctx.getFacesContext().getELContext(), new Object[]{parent});
+            TagAttribute attribute = getAttribute("listener");
+            if (attribute == null) {
+                throw new OctopusComponentUsageException("The tag scuredListener needs a listener atribute that is an EL expression to method with parameter of type UIComponent");
+            }
+            MethodExpression listener = JsfUtils.createMethodExpression(attribute.getValue(), Void.class, UIComponent.class);
+            try {
+                listener.invoke(ctx.getFacesContext().getELContext(), new Object[]{parent});
+            } catch (ELException e) {
+                throw new OctopusComponentUsageException(String.format("The value of listener atribute must be an EL expression to a method with parameter of type UIComponent. Found '%s'", attribute.getValue()));
+            }
         }
 
     }
